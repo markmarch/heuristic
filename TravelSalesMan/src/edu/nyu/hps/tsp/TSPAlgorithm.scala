610e1306a3451b8de4a3d757bcb667ca9c8d6e6a
mark_march <markofmarch@gmail.com>
2011-09-25 00:30:14 -0400

    Fixed calculating minmum spanning tree for graph. Added nearest insertion(not working now)."

---
+++ TravelSalesMan/src/edu/nyu/hps/tsp/TSPAlgorithm.scala
@@ -0,0 +1,58 @@
+package edu.nyu.hps.tsp
+
+object TSPAlgorithm {
+
+  // Find a tour using nearest insertion algorithm
+  def nearestInsertion(graph : Graph) = {
+    val sortedEdges = graph.edges.sortBy(_.weight)
+    val shortestEdge = sortedEdges.head
+    val subTour = new Tour(Set(shortestEdge.vertexA, shortestEdge.vertexB), Set(shortestEdge))
+
+    def findNearestVertex(sub : List[Vertex], rest : List[Vertex]) = {
+      var nearest = sub(0) distanceTo rest(0)
+      var selected = rest(0)
+      for (v <- sub; u <- rest) {
+        val temp = v distanceTo u
+        if (temp < nearest) {
+          nearest = temp
+          selected = u
+        }
+      }
+      selected
+    }
+
+    def findOptimalEdge(v : Vertex, edges : List[Edge], cost : Double, minCost : Double, edge : Edge) : Edge = {
+      edges match {
+        case Nil => edge
+        case e :: ls => {
+          val newCost = cost + replaceCost(v, e)
+          if (newCost < minCost)
+            findOptimalEdge(v, ls, cost, newCost, e)
+          else
+            findOptimalEdge(v, ls, cost, minCost, edge)
+        }
+      }
+    }
+
+    // cost increased to replace edge e wigh two new edges (e.vertexA, v) and
+    // (v, e.vertextB)
+    def replaceCost(v: Vertex, e :Edge) = {
+      v.distanceTo(e.vertexA) + v.distanceTo(e.vertexB) - e.weight
+    }
+    def findTour(subTour : Tour, rest : Set[Vertex]) : Tour = {
+      rest match {
+        case e if e.isEmpty => subTour
+        case _ => {
+          val v = findNearestVertex(subTour.vertices.toList, rest.toList)
+          val edge = new Edge(subTour.vertices.first, subTour.vertices.tail.first)
+          val minCost = subTour.cost + replaceCost(v, edge)
+          val optimalEdge = findOptimalEdge(v, subTour.edges.toList, subTour.cost, minCost,edge)
+          findTour(new Tour(subTour.vertices + v, subTour.edges + optimalEdge), rest - v)
+        }
+      }
+    }
+    
+    val tour = findTour(subTour, graph.vertices.toSet -- subTour.vertices)
+    tour.edges.toList
+  }
+}
--- TravelSalesMan/src/edu/nyu/hps/tsp/TravellingSalesmenProblem.scala
+++ TravelSalesMan/src/edu/nyu/hps/tsp/TravellingSalesmenProblem.scala
@@ -1,55 +1,96 @@
 package edu.nyu.hps.tsp
 import scala.collection.immutable.HashSet
+import scala.collection.immutable.TreeMap
 
 object TravellingSalesmenProblem {
   def main(args : Array[String]) : Unit = {
     import scala.io.Source
-    
-    val cities = (for(line <- Source.fromFile(args(0)).getLines()) yield createCity(line)).toList
-    cities.foreach(println)
+
+    val vertices = (for (line <- Source.fromFile(args(0)).getLines())
+      yield createVertex(line)).toList
+
+    val edges = for {
+      i <- 0 until vertices.length;
+      j <- i + 1 until vertices.length
+    } yield new Edge(vertices(i), vertices(j))
+
+    val graph = new Graph(vertices, edges.toList);
+    val selectedEdges = TSPAlgorithm.nearestInsertion(graph)
+    selectedEdges.foreach(println)
+
+    //    val mst = graph.getMST()
+    //
+    //    mst.edges.foreach((edge) => println(edge.vertexA.number + " - " + edge.vertexB.number + " : " + edge.weight))
+    //    val groupedEdges = mst.edges.groupBy {
+    //      case edge => edge.vertexA.number
+    //    }
+    //    val tmp = groupedEdges.toSeq.sortBy(_._1)
+    //    tmp.foreach((a) => println(a._1 + ":" + a._2.map(_.vertexB.number).mkString(",")))
+    //    TreeMap(groupedEdges.toSeq:_*).foreach((x) => println(x._1 + ": " + (x._2).map(_.cityB.number).mkString(",")))
   }
-  
-  def createCity(line : String) = {
+
+
+  // create a City object with given data as "city_number x y z"
+  def createVertex(line : String) = {
     val data = line.split(" ").flatMap((token : String) => Some(token.toInt))
-    new City(data(0), data(1), data(2), data(3))
+    new Vertex(data(0), data(1), data(2), data(3))
+  }
+
+  // time how long a call to function f takes
+  def time(f : => Unit) = {
+    val start = System.currentTimeMillis
+    f
+    val timeUsed = System.currentTimeMillis - start
+    println("Time used: " + timeUsed + " ms")
   }
 }
 
-class City(val number : Int, val x : Int, val y : Int, val z : Int) {
+class Vertex(val number : Int, val x : Int, val y : Int, val z : Int) {
   val cordinates = List(x, y, z)
-  def distanceTo(other : City) = City.distanceBetween(this, other)
-  
+  def distanceTo(other : Vertex) = Vertex.distanceBetween(this, other)
+
   override def toString() = {
     number + ": [" + cordinates.mkString(",") + "]"
   }
 }
 
-object City {
-  def distanceBetween(cityA : City, cityB : City) = {
-    Math.sqrt((cityA.cordinates, cityB.cordinates).zipped.map((a, b) => (a - b) * (a - b)).reduceLeft(_ + _))
+object Vertex {
+  // calculate distance between 2 cities
+  def distanceBetween(vA : Vertex, vB : Vertex) = {
+    Math.sqrt((vA.cordinates, vB.cordinates).zipped.map((a, b) => (a - b) * (a - b)).sum)
   }
 }
 
-class Edge(val cityA : City, val cityB : City) {
-  val weight = cityA.distanceTo(cityB)
+class Edge(val vertexA : Vertex, val vertexB : Vertex) {
+  val weight = vertexA.distanceTo(vertexB)
+
+  override def toString = vertexA.number + " --- " + vertexB.number + ", " + weight
 }
 
-class Graph(val cities : List[City], val edges : List[Edge]) {
-
+class Graph(val vertices : List[Vertex], val edges : List[Edge]) {
   // get the minimum spanning tree of this graph
   def getMST() = {
-    def addEdge(set : Set[City], selectedEdges : List[Edge], edgeList : List[Edge]) : List[Edge] = {
-      if (set.size == cities.length) selectedEdges
+    // continuous add the cheapest edge to selected edge list to form
+    // minimum spanning tree. Kruskal's Algorithm.
+    def addEdge(set : Set[Vertex], selectedEdges : List[Edge], edgeList : List[Edge]) : List[Edge] = {
+      if (set.size == vertices.length)
+        selectedEdges
       else {
         edgeList match {
-          case xs :: ls if !set.contains(xs.cityA) && !set.contains(xs.cityB) => xs :: edgeList
+          case xs :: ls if !set.contains(xs.vertexA) || !set.contains(xs.vertexB) => {
+            addEdge(set + xs.vertexA + xs.vertexB, xs :: selectedEdges, ls)
+          }
           case xs :: ls => addEdge(set, selectedEdges, ls)
           case Nil => selectedEdges
         }
       }
     }
-
-    val selectedEdges = addEdge(new HashSet[City], List(), edges.sortWith((e1, e2) => if (e1.weight < e2.weight) true else false))
-    new Graph(cities, selectedEdges)
+    val selectedEdges = addEdge(new HashSet[Vertex], List(),
+      edges.sortBy(_.weight))
+    new Graph(vertices, selectedEdges)
   }
+}
+
+class Tour(val vertices : Set[Vertex], val edges : Set[Edge]) {
+  val cost = edges.foldLeft(0.0)((sum, edge) => sum + edge.weight)
 }
